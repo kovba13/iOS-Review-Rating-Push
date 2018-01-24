28bb17ff3d10979d96e06cba7a50c25dacfc5ff3
Kostia Kovba <konstantinkovba@gmail.com>
2018-01-24 22:41:39 +0200

    Initial Commit

---
+++ iOS Review Rating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Review Rating
+//
+//  Created by Kostia Kovba on 24.01.18.
+//  Copyright © 2018 Kostia Kovba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
